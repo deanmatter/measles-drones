f88324b3597b66937cc69961140e8b4211294c39
deanmatter <deanmmatter@gmail.com>
2020-06-16 19:12:03 +0200

    Created pdf for scatter used in article

--- code/strategyComparisonPlot.py
+++ code/strategyComparisonPlot.py
@@ -15,57 +15,67 @@ def scatter_markers(vs, cs, s, col, ms, l):
         ls: list of scatter labels
     '''    
     for i in range(len(vs)):
-        v = vs.iloc[i]
-        c = cs.iloc[i]
-        m = ms[i]
-        print(v,c,s,col,m,l)
-        scatters.append(ax.scatter(v,c,s=50,color=col,marker=m,label=l))
+        v = vs.iloc[i]                                                      # num vaccines for this point
+        c = cs.iloc[i]                                                      # num cases for this point
+        m = ms[i]                                                           # the marker: like '$I$'
+        scatters.append(ax.scatter(v,c,s=50,color=col,marker=m,label=l))    # scatter a single point
+        sc_letters.append(vacc_strat_names[m])                              # append vacc strat name
 
 # Read in input from CSV
 df = pd.read_csv('results/strategies/strategy_comparisons.csv')
 df_capped = df[df['Delivery strategy'] != 'uncapped']
-print(df_capped)
 
 # Prepare the figure for scatters
 fig, ax = plt.subplots()
 scatters = []
+sc_letters = []
 ax.set_title("Comparison of team and vaccine resource allocation strategies")
-ax.set_ylabel("")
-ax.set_xlabel("")
+ax.set_ylabel("Total number of cases")
+ax.set_xlabel("Total number of vaccines given")
+ax.set_xlim(600000,850000)
 
 # Dicts for information
-ts_names = {
-    'I':'Infections',
+ts_names = {                        # Key: team allocation strategy shortname
+    'I':'Infections',               # Value: team allocation strategy name
     'S':'Susceptible',
     'N':'Population',
-    'EPE':'EPE Team Method',
+    'EPE':'EPE Team Allocations',
     'I/N':'Infection Ratio',
     'spread':'Spread'
 }
-team_colours = {
-    'I':'gold',
+team_colours = {                    # Key: team allocation strategy shortname
+    'I':'gold',                     # Value: scatter point colour
     'S':'crimson',
     'N':'k',
     'EPE':'forestgreen',
     'I/N':'darkorange',
     'spread':'cornflowerblue'
 }
-vacc_markers = {
-    'I':'$I$',
-    'S':'$S$',
-    'N':'$N$',
+vacc_markers = {                    # Key: vaccine delivery strategy shortname
+    'I':'x',                      # Value: scatter point marker
+    'S':'+',  
+    'N':'$P$',
     'EPE':'$E$',
-    'absI':'$i$',
-    'absS':'$s$',
-    'absN':'$n$',
+    'absI':'$I$',
+    'absS':'$S$',
+    'absN':'$N$',
+}
+vacc_strat_names = {                        # Key: scatter point marker
+    'x':"Infections / minute",       # Value: vaccine delivery strategy name
+    '+':"Susceptible / minute",
+    '$P$':"Population / minute",
+    '$E$':"EPE Vaccine Delivery",
+    '$I$':"Infections",
+    '$S$':"Susceptible",
+    '$N$':"Population"
 }
 
 # Iterate through input to scatter points
 for ts in ['I','S','N','EPE','I/N','spread']:
-    ts_df = df_capped[df_capped['Team strategy']==ts]
-    label_keys = ts_df['Delivery strategy'].values
-    label_values = [vacc_markers[key] for key in label_keys]
-    scatter_markers(
+    ts_df = df_capped[df_capped['Team strategy']==ts]           # list of team alc strat shortnames
+    label_keys = ts_df['Delivery strategy'].values              # list of vacc del strat shortnames
+    label_values = [vacc_markers[key] for key in label_keys]    # list of scatter point markers
+    scatter_markers(                                # Call the custom scatter_markers method
         vs=ts_df['Vaccinations'], 
         cs=ts_df['Cases'], 
         s=50,
@@ -74,22 +84,16 @@ for ts in ['I','S','N','EPE','I/N','spread']:
         l=ts_names[ts]
     )
     
-
-plt.legend()
-plt.show()
-quit()
-
-
-
+# Delivery strategy legend, made using the labels added when calling ax.scatter()
 handles, labels = plt.gca().get_legend_handles_labels()
 by_label = OrderedDict(zip(labels,handles))
 leg1 = ax.legend(by_label.values(), by_label.keys(), title="Delivery strategy (colour):")
 plt.gca().add_artist(leg1)
 
-handles, labels = scatters, teamStrat3
+# Team allocation strategy legend, made manually by adding scatter points and scatter markers to scatters,sc_letters
+handles, labels = scatters, sc_letters
 by_label = OrderedDict(zip(labels,handles))
-leg2 =ax.legend(by_label.values(), by_label.keys(), loc ='lower left', title="Team strategy (letter):")
+leg2 =ax.legend(by_label.values(), by_label.keys(), loc ='lower right', title="Team strategy (letter):")
 
-plt.legend()
+fig.savefig("results/strategies/team_vaccine_scatter.pdf",bbox_inches='tight')
 plt.show()
-
