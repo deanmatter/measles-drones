315d9152901e4fc90c046998bfd1a805183ef562
deanmatter <deanmmatter@gmail.com>
2020-06-16 07:49:57 +0200

    add inf vaccineStock to EPE team strat calculation

--- code/network.py
+++ code/network.py
@@ -543,11 +543,14 @@ def teamPreventedExposures(pod, teams, params):
     
     noTeamsPOD.teamsAtPOD = 0
     noTeamsPOD.maxVaccinationsPerDay = 0
-    
     afterTeamsPOD.teamsAtPOD = teams
     afterTeamsPOD.maxVaccinationsPerDay = afterTeamsPOD.vaccsPerTeamDay * afterTeamsPOD.teamsAtPOD
         
-    #vaccinate both PODs with the vaccines available at each
+    # Set vaccine stock to be infinite for test POD, so there's no dependency on it
+    afterTeamsPOD.vaccinesInStock = np.inf
+        
+    #vaccinate both PODs
+    #vaccinate both PODs 
     vaccinateOnePOD(noTeamsPOD)
     vaccinateOnePOD(afterTeamsPOD)
     
@@ -555,7 +558,7 @@ def teamPreventedExposures(pod, teams, params):
     progressSinglePOD(noTeamsPOD, params)
     progressSinglePOD(afterTeamsPOD, params)
     
-    #return the difference between exposure values - how many exposures did the flights prevent
+    #return the difference between exposure values
     return max(noTeamsPOD.E - afterTeamsPOD.E, 0)
   
   
