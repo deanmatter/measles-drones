b563962e2057bf43829db7a9c6896e610efc2c73
deanmatter <deanmmatter@gmail.com>
2020-06-16 12:59:44 +0200

    Plotting works, legend to be fixed

--- code/strategyComparisonPlot.py
+++ code/strategyComparisonPlot.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib as mpl
 mpl.rcParams['figure.dpi'] = 150
 import pandas as pd
+from collections import OrderedDict
 
 def scatter_markers(vs, cs, s, col, ms, l):
     '''Arguments:
@@ -12,25 +13,27 @@ def scatter_markers(vs, cs, s, col, ms, l):
         col: colour of scatter point
         ms: list of scatter markers
         ls: list of scatter labels
-    '''
-    
+    '''    
     for i in range(len(vs)):
         v = vs.iloc[i]
         c = cs.iloc[i]
         m = ms[i]
         print(v,c,s,col,m,l)
-        plt.scatter(v,c,s=50,color=col,marker=m,label=l) 
+        scatters.append(ax.scatter(v,c,s=50,color=col,marker=m,label=l))
 
+# Read in input from CSV
 df = pd.read_csv('results/strategies/strategy_comparisons.csv')
 df_capped = df[df['Delivery strategy'] != 'uncapped']
 print(df_capped)
 
-#plt.xlim()
-#plt.ylim()
-plt.title("Comparison of team and vaccine resource allocation strategies")
-plt.ylabel("")
-plt.xlabel("")
+# Prepare the figure for scatters
+fig, ax = plt.subplots()
+scatters = []
+ax.set_title("Comparison of team and vaccine resource allocation strategies")
+ax.set_ylabel("")
+ax.set_xlabel("")
 
+# Dicts for information
 ts_names = {
     'I':'Infections',
     'S':'Susceptible',
@@ -57,6 +60,7 @@ vacc_markers = {
     'absN':'$n$',
 }
 
+# Iterate through input to scatter points
 for ts in ['I','S','N','EPE','I/N','spread']:
     ts_df = df_capped[df_capped['Team strategy']==ts]
     label_keys = ts_df['Delivery strategy'].values
@@ -70,6 +74,21 @@ for ts in ['I','S','N','EPE','I/N','spread']:
         l=ts_names[ts]
     )
     
+
+plt.legend()
+plt.show()
+quit()
+
+
+
+handles, labels = plt.gca().get_legend_handles_labels()
+by_label = OrderedDict(zip(labels,handles))
+leg1 = ax.legend(by_label.values(), by_label.keys(), title="Delivery strategy (colour):")
+plt.gca().add_artist(leg1)
+
+handles, labels = scatters, teamStrat3
+by_label = OrderedDict(zip(labels,handles))
+leg2 =ax.legend(by_label.values(), by_label.keys(), loc ='lower left', title="Team strategy (letter):")
 
 plt.legend()
 plt.show()
